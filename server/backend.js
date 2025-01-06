2ff58f0fe367630bacae186d44082c284cb3bc84
dhanushgubba <dhanushgubba.ap@gmail.com>
2025-01-06 06:46:14 +0530

    commit

--- server/backend.js
+++ server/backend.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running on the port number ${PORT}`));
 
 const mongoUrl =
-  'mongodb+srv://admin:admin@cluster0.2lfz3.mongodb.net/Meghawebsite';
+  'mongodb+srv://gubbadhanush:admin@project0.g5i6y.mongodb.net/?retryWrites=true&w=majority&appName=Project0';
 /*const mongoUrl = 'mongodb://localhost:27017';*/
 const client = new MongoClient(mongoUrl);
 
