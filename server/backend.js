64a323032273bc7e84a5081c0b78ca8156baf0c0
dhanushgubba <dhanushgubba.ap@gmail.com>
2025-01-15 20:28:02 +0530

    commit

--- server/backend.js
+++ server/backend.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running on the port number ${PORT}`));
 
 const uri =
-  'mongodb+srv://dhanush:dhanush@cluster0.ar7z0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+  'mongodb+srv://dhanush:dhanush@cluster0.ar7z0.mongodb.net/Meghawebsite?retryWrites=true&w=majority&appName=Cluster0';
 const client = new MongoClient(uri);
 
 app.get('/klef/test', async function (req, res) {
