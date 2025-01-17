411925fbc99da7b88b6454c397d61a22583cbf61
dhanushgubba <dhanushgubba.ap@gmail.com>
2025-01-17 21:16:19 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -51,6 +51,7 @@ const App = () => {
   const showAdminNavbar = [
     '/admindashboard',
     '/adminevents',
+    '/adminvouchers',
     '/admincertifications',
   ].includes(location.pathname);
 
